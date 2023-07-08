4a0f21c63ca43ea64538ae7f70e925121b6b7cd0
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-08 09:49:08 +0800

    Update GameRendererMixin.java

--- src/main/java/net/krlite/flash/mixin/GameRendererMixin.java
+++ src/main/java/net/krlite/flash/mixin/GameRendererMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
+	@Unique
 	private MatrixStack matrixStack;
 
 	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;render(Lnet/minecraft/client/util/math/MatrixStack;F)V"), index = 0)
@@ -23,7 +25,11 @@ public class GameRendererMixin {
 
 	@Inject(method = "render", at = @At("RETURN"))
 	private void renderScreenshotFlash(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
-		if (!MinecraftClient.getInstance().skipGameRender && Flash.available() && matrixStack != null) {
+		if (
+				MinecraftClient.getInstance().world != null && MinecraftClient.getInstance().currentScreen == null
+						&& !MinecraftClient.getInstance().skipGameRender
+						&& Flash.available() && matrixStack	!= null
+		) {
 			matrixStack.push();
 			matrixStack.translate(MinecraftClient.getInstance().getWindow().getScaledWidth() / 2.0, MinecraftClient.getInstance().getWindow().getScaledHeight() / 2.0, 0);
 
